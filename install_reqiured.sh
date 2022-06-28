df3cc2e12707a7b5a3b20242d152081166ce1b8b
Iamjava <jan.vaorin@tu-dortmund.de>
2022-06-28 12:01:30 +0200

    added shell

---
+++ install_reqiured.sh
@@ -0,0 +1,2 @@
+apt-get install texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
+git clone https://github.com/maxnoe/tudobeamertheme $(kpsewhich --var-value TEXMFHOME)/tex/latex/tudobeamertheme
